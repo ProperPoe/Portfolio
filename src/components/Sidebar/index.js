9c6f0ba4560d247cb64f3108b1fb0379b05246c4
Steven Trujillo <potencyalmighty@outlook.com>
2022-10-27 13:23:00 -0600

    Added fontawesome icons for sidebar links

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,21 +1,36 @@
 import React from 'react'
 import { Link, NavLink } from 'react-router-dom'
 import './index.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHome, faComputer, faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons'
 
 function Sidebar() {
     return (
         <div className='nav-bar'>
             <nav>
                 <NavLink exact="true" activeclassname="active" to="/">
-                    Home
+                    <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
                 </NavLink>
                 <NavLink exact="true" activeclassname="active" className="projects-link" to="/projects">
-                    Projects
+                    <FontAwesomeIcon icon={faComputer} color="#4d4d4e" />
                 </NavLink>
                 <NavLink exact="true" activeclassname="active" to="/contact">
-                    Contact
+                    <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
                 </NavLink>
             </nav>
+            <ul>
+                <li>
+                    <a target="_blank" rel="noreferrer" href="https://github.com/ProperPoe">
+                        <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+                    </a>
+                </li>
+                <li>
+                    <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/">
+                        <FontAwesomeIcon icon={faLinkedinIn} color="#4d4d4e" />
+                    </a>
+                </li>
+            </ul>
         </div>
     )
 }
